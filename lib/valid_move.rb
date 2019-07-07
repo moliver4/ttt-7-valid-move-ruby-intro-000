59cba305e70d57b9f7f6fb622342a5d301811539
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 00:49:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if position_taken? == true && between(0, 8) == true
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
