c33effc5aaf5fecd85d9be707af480037195c308
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 00:56:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken? && index.between(0, 8)
     true
-  elsif (position_taken? != true) && index.between(0, 8)
+  elsif position_taken? != true && index.between(0, 8)
     true
   else
     false
